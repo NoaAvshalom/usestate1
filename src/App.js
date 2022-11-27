5cd0438f4bbf270f2cb03c4401e185691e84a0fc
Noa Avshalom <noavshalom@gmail.com>
2022-11-27 16:43:59 +0200

    Added max

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import './App.css';
 import Counter from './Counter';
+import { useState } from 'react';
+
 
 function App() {
+  const [delta, setDelta] = useState(1)
+  const [max , setMax] =useState(1)
+
+function handleDelta(e){
+  console.log(e);
+  setDelta(Number(e.target.value))
+}
+function handleMax(e){
+  setMax(Number(e.target.value))
+}
   return (
     <div className="App">
-     
-     <Counter/>
+      useState
+     <input type = "number" value = {delta} onChange = {handleDelta}/>
+     <input type = "number" value = {max} onChange = {handleMax}/>
+     <Counter delta = {delta} max ={max}/>
+     <Counter delta = {delta} max ={max}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,27 @@
 import { useState } from "react";
 
-function Counter() {
+function Counter(props) {
+    const {delta} = props
     const [count, setCount] = useState(1)
-    const [delta, setDelta] = useState(1)
+    const {max} = props
     function incr(){
         setCount(
             function(oldCount){
+                if ((oldCount+ delta)<= max){
                 return oldCount + delta
             }
+            else {return 0}
+        }    
         )
     }
     function reset(){
         setCount(0)
         
     }
-    function handleDelta(e){
-        console.log(e);
-        setDelta(Number(e.target.value))
-    }
-        
+    
     return (
       <div >
       <h1>Counter</h1>
-      <input type = "number" value = {delta} onChange = {handleDelta}/>
       <p>Counter is at {count}</p>
       <button onClick={incr}> Click to add {delta} to Counter</button>
       <button onClick={reset}> Click to reset Counter</button>
